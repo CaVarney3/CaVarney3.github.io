de419299cafb388feb5d0927e41671178931379f
Connor <126129332+CaVarney3@users.noreply.github.com>
2024-05-01 14:45:15 -0400

    Add files via upload

---
+++ AppMtBike/js/scripts.js
@@ -0,0 +1,22 @@
+/*scripts.js
+By Connor Varney
+*/
+
+
+//Function to display the 1st answer
+function ans1() {
+    heading.style.display = "block";
+    answer.textContent = "Yes!";
+}
+
+//Function to display the 2nd answer
+function ans2() {
+    heading.style.display = "block";
+    answer.textContent = "Yes!";
+}
+
+//Function to display the 3rd answer
+function ans3() {
+    heading.style.display = "block";
+    answer.textContent = "Yes!";
+}
